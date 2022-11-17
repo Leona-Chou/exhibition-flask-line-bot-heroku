1992ae066b462ea867ec599e6f7604affd1d579c
Leona-Chou <zst1052970655@gmail.com>
2022-11-17 21:30:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # https://github.com/line/line-bot-sdk-python/blob/master/examples/flask-echo/app_with_handler.py
 
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import sys
 from argparse import ArgumentParser
 
@@ -19,7 +19,7 @@ from linebot.models import (
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
-load_dotenv()
+# load_dotenv()
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 if channel_secret is None:
