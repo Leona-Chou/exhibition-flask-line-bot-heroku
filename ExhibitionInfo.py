7d832b201a2077ca91e96330345420d5ed6a048b
Leona-Chou <zst1052970655@gmail.com>
2022-11-19 20:32:39 +0800

    Update ExhibitionInfo.py

--- ExhibitionInfo.py
+++ ExhibitionInfo.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 
 CKSMH_URL = 'https://www.cksmh.gov.tw/activitysoonlist_369'  # 中正紀念堂展館
 
+
 def CheckUpdate():
-    CurrentExhibition_Info = GetExihibitionInfo()
+    CurrentExhibitionInfo = GetExihibitionInfo()
 
 
 
@@ -13,14 +14,14 @@ def GetExihibitionInfo():
 
     # 中正紀念堂展館
     for Page in range(1, 3):
-        URL = f'{CKSMH_URL}{Page}.html'
+        URL = f'{CKSMH_URL}_{Page}.html'
         Response = requests.get(URL)
-        Soup = BeautifulSoup(Response.content, 'html.parser')
+        Soup = BeautifulSoup(Response.content, "html.parser")
 
         # 從 HTML 抓取內容
         Titles = Soup.find_all('div', class_='h3')  # 展名
         Dates = Soup.find_all('span', class_='date')  # 展期
-        Location = Soup.find_all('span', class_='location')  # 地點
+        Locations = Soup.find_all('span', class_='location')  # 地點
 
         # 處理資料並存入 dictionary
         for i in range(len(Titles)):
@@ -30,18 +31,21 @@ def GetExihibitionInfo():
             StartDate = DateList[2]
             EndDate = DateList[3]
             DateTime = DateList[4]
-            LocationList = Location[i].text.strip('地點：')
+            Location = Locations[i].text.strip('地點：')
             Link = Soup.find('a', title=Title+'「另開新視窗」').get('href')
+            # PictureLinkTmp = Soup.select(f'#fontsize > div:nth-child(3) > ul > li:nth-child({i+1}) > dl > dt > a > span > img')
+            ImgLinkTmp = Soup.find('ul', class_='exhibition-list')
+            ImgLink = ImgLinkTmp.find('img').get('src')
 
             Dict = {
                 'Title': Title,  # 展名
                 'StartDate': StartDate,  # 起始日
                 'EndDate': EndDate,  # 結束日
                 'Time': DateTime,  # 時間
-                'Location': LocationList,  # 地點
-                'Link': Link
+                'Location': Location,  # 地點
+                'ExhibitionLink': Link,  # 連結
+                'ImgLink': ImgLink  # 圖片
             }
             ExihibitionList.append(Dict)
 
     return ExihibitionList
-    # print(ExihibitionList)
