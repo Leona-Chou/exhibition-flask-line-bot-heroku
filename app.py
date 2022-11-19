4749d52a9ca3b8d3c3ccc58410c02121bb6ef79b
Leona-Chou <zst1052970655@gmail.com>
2022-11-19 20:32:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import os
 # from dotenv import load_dotenv
 import sys
 from argparse import ArgumentParser
-
 from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
@@ -16,6 +15,8 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,
 )
 from werkzeug.debug import console
+
+import ExhibitionMongo
 
 app = Flask(__name__)
 
@@ -55,23 +56,24 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
     user_id = event.source.user_id
-    console.log('使用者ID：', event.source.user_id)
-    print('user_id:' + user_id)
+    console.log('使用者ID：', user_id)
+
+    # 存取user_id
+    ExhibitionMongo.AddUserId(user_id)
 
     # line_bot_api.reply_message(event.reply_token, TextSendMessage(text="reply: "+event.message.text))
-    if event.message.text == 'test':  # 測試
+    if event.message.text == 'test':  # 測試 text
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text + ' success!'))
         print('test success')
-        # line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text + ' success!'))
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='id: ' + user_id))
-    elif event.message.text == 'picture':
-        print('picture get')
+    elif event.message.text == 'picture': # 測試 picture
         line_bot_api.reply_message(event.reply_token, ImageSendMessage(
             original_content_url='https://mocfile.moc.gov.tw/activitySones/userFiles/CKSMH/JpgFile/01/04758/04758.jpg',
             preview_image_url='https://mocfile.moc.gov.tw/activitySones/userFiles/CKSMH/JpgFile/01/04758/04758.jpg'
         ))
+        print('picture get')
     else:
-        print('else')
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="reply: " + event.message.text))
+        print('else')
 
 
 if __name__ == "__main__":
