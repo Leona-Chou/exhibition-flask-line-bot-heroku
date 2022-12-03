400523a063c8992e892fe3cf45a9b79dac9a3a32
Leona-Chou <zst1052970655@gmail.com>
2022-12-03 14:20:29 +0800

    Update ExhibitionInfo.py

--- ExhibitionInfo.py
+++ ExhibitionInfo.py
@@ -29,9 +29,9 @@ def GetExihibitionInfo():
             DateTime = DateList[4]
             Location = Locations[i].text.strip('地點：')
             Link = Soup.find('a', title=Title+'「另開新視窗」').get('href')
-            # PictureLinkTmp = Soup.select(f'#fontsize > div:nth-child(3) > ul > li:nth-child({i+1}) > dl > dt > a > span > img')
-            # ImgLinkTmp = Soup.find('ul', class_='exhibition-list')
-            # ImgLink = ImgLinkTmp.find('img').get('src')
+            PictureLinkTmp = Soup.select(f'#fontsize > div:nth-child(3) > ul > li:nth-child({i+1}) > dl > dt > a > span > img')
+            ImgLinkTmp = Soup.find('ul', class_='exhibition-list')
+            ImgLink = ImgLinkTmp.find('img').get('src')
 
             Dict = {
                 'Title': Title,          # 展名
@@ -39,8 +39,8 @@ def GetExihibitionInfo():
                 'EndDate': EndDate,      # 結束日
                 'Time': DateTime,        # 時間
                 'Location': Location,    # 地點
-                'ExhibitionLink': Link   # 連結
-                # 'ImgLink': ImgLink       # 圖片
+                'ExhibitionLink': Link,   # 連結
+                'ImgLink': ImgLink       # 圖片
             }
             ExihibitionList.append(Dict)
 
