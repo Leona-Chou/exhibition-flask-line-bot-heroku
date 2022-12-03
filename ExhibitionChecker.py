6feb9e8443a7742e73c028943ae02f4ab8e79cce
Leona-Chou <zst1052970655@gmail.com>
2022-12-03 16:50:58 +0800

    Update ExhibitionChecker.py

--- ExhibitionChecker.py
+++ ExhibitionChecker.py
@@ -102,7 +102,7 @@ sched = BlockingScheduler()
 
 # 每日執行
 # sched.add_job(CheckExhibition, args=(ExhibitionList,), trigger='cron', id='CheckExhibition_job', hour=3, minute=30)  # 每日10:30執行
-sched.add_job(CheckExhibition, args=(ExhibitionList,), trigger='cron', id='CheckExhibition_job', hour=16, minute=47)  # test
+sched.add_job(CheckExhibition, args=(ExhibitionList,), trigger='cron', id='CheckExhibition_job', hour=16, minute=53)  # test
 # sched.add_job(CheckExhibition, args=(ExhibitionList,), trigger='interval', id='CheckExhibition_job', minutes=1)  # test
 
 # 防止自動休眠
