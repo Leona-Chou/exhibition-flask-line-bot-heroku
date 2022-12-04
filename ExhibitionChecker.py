06a8ff31ccbe9b2b8b19abaf364a1a789212bab5
Leona-Chou <zst1052970655@gmail.com>
2022-12-04 20:47:28 +0800

    Update ExhibitionChecker.py

--- ExhibitionChecker.py
+++ ExhibitionChecker.py
@@ -85,7 +85,7 @@ def DoNotSleep():
 sched = BlockingScheduler(timezone="Asia/Taipei")
 
 # 每日執行
-sched.add_job(CheckExhibition, args=(ExhibitionList,), trigger='cron', id='CheckExhibition_job', hour=16, minute=30)  # 每日10:30執行
+sched.add_job(CheckExhibition, args=(ExhibitionList,), trigger='cron', id='CheckExhibition_job', hour=20, minute=49)  # 每日10:30執行
 
 # 防止自動休眠
 sched.add_job(DoNotSleep, trigger='interval', id='DoNotSleeps_job', minutes=20)
