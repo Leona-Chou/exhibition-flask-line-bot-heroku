0c6dac48a4ff02ba390d18938cc4c2db041a0fbe
Leona-Chou <zst1052970655@gmail.com>
2022-12-05 22:22:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,4 +8,4 @@ channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 line_bot_api = LineBotApi(channel_access_token)
 Users = ExhibitionMongo.GetUserId()
 for User in Users:
-    line_bot_api.push_message(User["User_Id"], TextSendMessage(text='安安，目前正在測試，如果有跳出任何怪怪的訊息請不要理它~'))
+    line_bot_api.push_message(User["User_Id"], TextSendMessage(text='安安~'))
